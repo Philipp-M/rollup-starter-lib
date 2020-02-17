43e98978436d630a7b0b45488936d7fcf7d98da0
Philipp Mildenberger <philipp.mildenberger@artfabrik.at>
2020-02-17 19:13:58 +0100

    update dependencies to latest version and use typescript transpilation

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,22 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
 import pkg from './package.json';
 
 export default [
 	// browser-friendly UMD build
 	{
-		input: 'src/main.js',
+		input: 'src/main.ts',
 		output: {
 			name: 'howLongUntilLunch',
 			file: pkg.browser,
-			format: 'umd'
+			format: 'umd',
+			sourcemap: true
 		},
 		plugins: [
-			resolve(), // so Rollup can find `ms`
-			commonjs() // so Rollup can convert `ms` to an ES module
+			resolve({extensions: ['.ts', '.mjs', '.js', '.json', '.node']}), // so Rollup can find `ms`
+			typescript(),
+			commonjs(), // so Rollup can convert `ms` to an ES module
 		]
 	},
 
@@ -24,11 +27,14 @@ export default [
 	// an array for the `output` option, where we can specify
 	// `file` and `format` for each target)
 	{
-		input: 'src/main.js',
+		input: 'src/main.ts',
 		external: ['ms'],
 		output: [
 			{ file: pkg.main, format: 'cjs' },
 			{ file: pkg.module, format: 'es' }
+		],
+		plugins: [
+			typescript()
 		]
 	}
 ];
